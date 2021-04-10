8a0903fe3cd5291a60753a30a594fb1444b5a8e3
dev-talha-anwar <786anwartalha@gmail.com>
2021-04-10 08:10:25 -0700

    fixed tab issue

--- background.js
+++ background.js
@@ -1,11 +1,10 @@
-chrome.runtime.onInstalled.addListener(function() {
-	chrome.action.onClicked.addListener((tab) => {
-		chrome.tabs.executeScript({
-			file: '/contentScripts/getHostName.js'
+chrome.action.onClicked.addListener((tab) => {
+	chrome.scripting.executeScript({
+		target: {tabId: tab.id},
+		files: ['/contentScripts/getHostName.js']
 		});
 		chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-			let url = chrome.runtime.getURL("/pages/index/index.html?shop="+message.hostname);
-			chrome.tabs.create({ url: url });
-		});
+		let url = chrome.runtime.getURL("/pages/index/index.html?shop="+message.hostname);
+		chrome.tabs.create({ url: url });
 	});
-})
+});
